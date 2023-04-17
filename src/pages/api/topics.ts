314a5f2a6242c98962feaca75d57fb7c03671bd2
Rohit Patil <rahulhimesh09@gmail.com>
2023-04-18 00:43:48 +0530

    feat: add endpoint to get topics for a subject

---
+++ src/pages/api/topics.ts
@@ -0,0 +1,45 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+/**
+ * Get Route to get all unqiue topics for a subject passed in as a query param
+ *
+ * this route first calls the graphql api to get all the topics for a subject
+ * then it filters out the duplicates and returns the unique topics
+ */
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const subject = req.query.subject as string;
+  const questions = await fetch(
+    "https://api.thegraph.com/subgraphs/name/aditipolkam/qrate97",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+          {
+            questions(where: {question_subject: "${subject}", question_status: 1}) {
+              question_topic
+            }
+          }
+          `,
+      }),
+    }
+  );
+  const json_results: { data: { questions: { question_topic: string }[] } } =
+    await questions.json();
+  const results = json_results;
+  const topics = results.data.questions.map(
+    (question) => question.question_topic
+  );
+  const uniqueTopics: string[] = [];
+  for (const topic of topics) {
+    if (!uniqueTopics.includes(topic)) {
+      uniqueTopics.push(topic);
+    }
+  }
+  res.status(200).json(uniqueTopics);
+}
